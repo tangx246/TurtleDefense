5b8e167c52904ce1e1ff13cba01a22008df5c7bc
Yee Chien Tang <tangx246@umn.edu>
2023-07-19 16:04:47 -0700

    Animated eggs

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class Egg : MonoBehaviour
 {
     public float waitTimeSeconds;
+    public float hatchAnimationTimeSeconds = 0.458f;
     public GameObject turtlePrefab;
+
+    private new Animation animation;
+
+    private void Awake()
+    {
+        animation = GetComponentInChildren<Animation>();
+    }
 
     void Start()
     {
         // Wait for waitTime seconds and then spawn the turtle
         Invoke("SpawnTurtle", waitTimeSeconds);
+        Invoke("PlayHatchAnimation", waitTimeSeconds - hatchAnimationTimeSeconds);
     }
 
     void SpawnTurtle()
@@ -17,5 +26,10 @@ public class Egg : MonoBehaviour
         Instantiate(turtlePrefab, transform.position, transform.rotation);
         // Destroy the egg
         Destroy(gameObject);
+    }
+
+    private void PlayHatchAnimation()
+    {
+        animation.Play(PlayMode.StopAll);
     }
 }

b4a6bdadc99d5eb3ef51ed4d669434cb21ce1cb9
Yee Chien Tang <tangx246@umn.edu>
2023-07-19 15:16:48 -0700

    Added pit trap counts

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,12 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public LayerMask groundLayer;
 
+    [Header("Pit Trap")]
     public GameObject pitTrapPrefab;
+    public int pitTrapCount = 1;
+    public TMP_Text pitTrapText;
 
-    public GameObject trapOnCursor = null;
+    [SerializeField] private GameObject trapOnCursor = null;
+    [SerializeField] private Trap currentTrapType = Trap.None;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
 
     private void Update()
     {
@@ -25,20 +35,46 @@ public class Player : MonoBehaviour
 
     public void StartPlacingPitTrap()
     {
-        trapOnCursor = Instantiate(pitTrapPrefab);
+        if (pitTrapCount > 0)
+        {
+            trapOnCursor = Instantiate(pitTrapPrefab);
+            currentTrapType = Trap.Pit;
+        }
     }
 
     public void PlaceCurrentTrap()
     {
-        Debug.Log("Placing trap");
-        var trap = Instantiate(pitTrapPrefab, trapOnCursor.transform.position, trapOnCursor.transform.rotation);
-        ActivateTrap(trap);
-        Destroy(trapOnCursor);
-        trapOnCursor = null;
+        if (currentTrapType != Trap.None)
+        {
+            Debug.Log("Placing trap");
+            switch (currentTrapType)
+            {
+                case Trap.Pit:
+                    pitTrapCount--;
+                    break;
+            }
+            var trap = Instantiate(pitTrapPrefab, trapOnCursor.transform.position, trapOnCursor.transform.rotation);
+            ActivateTrap(trap);
+            Destroy(trapOnCursor);
+            trapOnCursor = null;
+            UpdateUI();
+        }
     }
 
     private void ActivateTrap(GameObject go)
     {
         go.GetComponentInChildren<TurtleDestroyer>().enabled = true;
+        currentTrapType = Trap.None;
+    }
+
+    private void UpdateUI()
+    {
+        pitTrapText.text = pitTrapCount.ToString();
+    }
+
+    private enum Trap 
+    { 
+        None,
+        Pit
     }
 }

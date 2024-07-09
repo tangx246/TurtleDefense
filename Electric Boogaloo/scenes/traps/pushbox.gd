7c2685c445256f9288da5b0060b44a9e5fa33600
Yee Chien Tang <tychien99@msn.com>
2024-07-09 14:28:48 -0700

    Simplify trigger mechanism of pushbox by using call_deferred

--- Electric Boogaloo/scenes/traps/pushbox.gd
+++ Electric Boogaloo/scenes/traps/pushbox.gd
@@ -3,24 +3,19 @@ extends Node3D
 @export var active : bool = true
 @export var area : Area3D
 
-var trigger : bool = false
-
-func _physics_process(_delta):
-	if trigger:
-		var overlappingBodies = area.get_overlapping_bodies()
-		for overlappingBody in overlappingBodies:
-			if overlappingBody.get_parent() is Turtle:
-				var turtleBody : MovementAgent = overlappingBody
-				var tween = create_tween()
-				tween.tween_property(turtleBody, "position", position + basis * Vector3.BACK * 5, 0.2)
-				active = false
-		
-		trigger = false
-
 func _on_area_3d_body_entered(body):
 	if !active:
 		return
 
 	var bodyParent : Node3D = body.get_parent() as Turtle
 	if bodyParent:
-		trigger = true
+		push_turtles.call_deferred()
+
+func push_turtles():
+	var overlappingBodies = area.get_overlapping_bodies()
+	for overlappingBody in overlappingBodies:
+		if overlappingBody.get_parent() is Turtle:
+			var turtleBody : MovementAgent = overlappingBody
+			var tween = create_tween()
+			tween.tween_property(turtleBody, "position", position + basis * Vector3.BACK * 5, 0.2)
+			active = false

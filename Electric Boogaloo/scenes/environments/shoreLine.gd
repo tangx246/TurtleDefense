a78b7a4dbf3af24b511290e524d0c0e527f21e1a
Yee Chien Tang <tychien99@msn.com>
2024-07-15 11:34:08 -0700

    Clean up shoreLine logic

--- Electric Boogaloo/scenes/environments/shoreLine.gd
+++ Electric Boogaloo/scenes/environments/shoreLine.gd
@@ -4,8 +4,9 @@ extends Node3D
 signal turtle_escaped
 
 func _on_area_3d_body_entered(body : Node3D):
-	if not body.get_parent() is Turtle:
+	var turtle = body.get_parent() as Turtle
+	if not turtle:
 		return
 	
 	turtle_escaped.emit()
-	body.get_parent().queue_free()
+	turtle.queue_free()

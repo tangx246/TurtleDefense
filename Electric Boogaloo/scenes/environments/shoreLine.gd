da127e67defdf4365c258d1ffe13a7887ac139e0
Yee Chien Tang <tychien99@msn.com>
2024-07-15 11:28:43 -0700

    Shoreline now checks specifically for Turtles when escaping things

--- Electric Boogaloo/scenes/environments/shoreLine.gd
+++ Electric Boogaloo/scenes/environments/shoreLine.gd
@@ -4,5 +4,8 @@ extends Node3D
 signal turtle_escaped
 
 func _on_area_3d_body_entered(body : Node3D):
+	if not body.get_parent() is Turtle:
+		return
+	
 	turtle_escaped.emit()
 	body.get_parent().queue_free()

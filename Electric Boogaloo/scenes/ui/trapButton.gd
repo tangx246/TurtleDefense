347e8c9c4bf00681b43651a25f5520839be26f2a
Yee Chien Tang <tychien99@msn.com>
2024-07-15 15:19:35 -0700

    Created a proper trap button

---
+++ Electric Boogaloo/scenes/ui/trapButton.gd
@@ -0,0 +1,24 @@
+@tool
+extends MarginContainer
+
+signal pressed_item(item: PlacedItem)
+
+@export var item : PlacedItem
+@export var icon : Texture2D
+@export var label : String
+@export var topdownItemPlacer : TopdownItemPlacer
+@export var click : AudioStreamPlayer
+
+@onready var text : Label = %Label
+@onready var textureRect : TextureRect = %TextureRect
+@onready var button : PlacedItemButton = %Button
+
+func _ready():
+	button.item = item
+	text.text = label
+	textureRect.texture = icon
+
+func _on_button_pressed_item(_item: PlacedItem):
+	pressed_item.emit(item)
+	topdownItemPlacer.place_item_requested(item)
+	click.play()
